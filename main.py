a3514afa8256f3d402a62364ce89654abf60b9b7
Burcu <burcubaycan96@gmail.com>
2021-08-03 01:55:29 +0200

    Deleted some dead code

--- main.py
+++ main.py
@@ -67,15 +67,6 @@ class Comment(db.Model):
 db.create_all()
 
 
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if current_user.id != 1:
-            return abort(403)
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
@@ -170,11 +161,10 @@ def contact():
 
 
 @app.route("/new-post", methods=["GET", "POST"])
-# @admin_only
 def add_new_post():
-    form = CreatePostForm()
+    form = CreatePostForm(
     if form.validate_on_submit():
-        new_post = BlogPost(
+        new_post=BlogPost(
             title=form.title.data,
             subtitle=form.subtitle.data,
             body=form.body.data,
@@ -186,54 +176,15 @@ def add_new_post():
         db.session.commit()
         return redirect(url_for("get_all_posts"))
     return render_template("make-post.html", form=form)
-    # if form.validate_on_submit():
-    # new_post = BlogPost(
-    #     title=form.title.data,
-    #     subtitle=form.subtitle.data,
-    #     body=form.body.data,
-    #     img_url=form.img_url.data,
-    #     author=current_user,
-    #     date=date.today().strftime("%B %d, %Y")
-    # )
-    # db.session.add(new_post)
-    # db.session.commit()
-    # return redirect(url_for("get_all_posts"))
 
-    # return render_template("make-post.html", form=form, current_user=current_user)
-    # return render_template("make-post.html", form=form)
-
-
-@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-# @admin_only
-def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        img_url=post.img_url,
-        author=current_user,
-        body=post.body
-    )
-    if edit_form.validate_on_submit():
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.body = edit_form.body.data
-        db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
-
-    return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
-
-
-@app.route("/delete/<int:post_id>")
-# @admin_only
+@ app.route("/delete/<int:post_id>")
 def delete_post(post_id):
-    post_to_delete = BlogPost.query.get(post_id)
+    post_to_delete=BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
-    app.debug = True
+    # app.debug = True
     app.run(host='0.0.0.0', port=5000)
